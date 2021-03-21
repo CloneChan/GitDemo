233986a5ec68247c1c434d1f61316495840e6c1f
conan chan <15920025414@qq.com>
2021-03-21 21:00:25 +0800

    the second commit  from dev

---
+++ src/main/java/com/tencent/GitMini.java
@@ -0,0 +1,14 @@
+package com.tencent;
+
+/**
+ * GitMini.java
+ *
+ * @Author: CZQ
+ * @CreateTime: 19:32-2021/3/21
+ * @Description:
+ */
+public class GitMini {
+    public static void main(String[] args) {
+        System.out.println("i am hungry");
+    }
+}
