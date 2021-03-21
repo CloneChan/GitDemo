98c4ca8c7e72e2198c772ec6a3372cbadbfc2d8b
conan chan <15920025414@qq.com>
2021-03-21 18:18:27 +0800

    this commit is from dev

---
+++ src/main/java/com/tencent/GitDemo.java
@@ -0,0 +1,18 @@
+package com.tencent;
+
+/**
+ * GitDemo.java
+ *
+ * @Author: CZQ
+ * @CreateTime: 17:47-2021/3/21
+ * @Description:
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+        System.out.println("commit and checkout");
+    }
+}
