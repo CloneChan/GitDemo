384409e46f1f3091d8185b6663650509b1ea0477
conan chan <15920025414@qq.com>
2021-03-25 20:42:26 +0800

    dev local commit GitDemo.java 14l

--- src/main/java/com/tencent/GitDemo.java
+++ src/main/java/com/tencent/GitDemo.java
@@ -11,7 +11,7 @@ public class GitDemo {
 
     public static void main(String[] args) {
 
-        System.out.println("hello kara");
+        System.out.println("hello betty");
 
         System.out.println("commit and checkout");
     }
