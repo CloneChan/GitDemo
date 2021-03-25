e6cb60d38eb225391791a8e3355487961b1f1b54
CloneChan <80335463+CloneChan@users.noreply.github.com>
2021-03-25 21:51:27 +0800

    Update GitDemo.java
    conan commit

--- src/main/java/com/tencent/GitDemo.java
+++ src/main/java/com/tencent/GitDemo.java
@@ -11,7 +11,7 @@ public class GitDemo {
 
     public static void main(String[] args) {
 
-        System.out.println("hello andy");
+        System.out.println("hello conan");
         System.out.println("hello tony");
         System.out.println("hello passion");
 
