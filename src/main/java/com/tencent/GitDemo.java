d16b6b3798db67b68fb1cfe69688305810370809
CloneChan <80335463+CloneChan@users.noreply.github.com>
2021-03-25 21:08:27 +0800

    Update GitDemo.java
    remote second commit

--- src/main/java/com/tencent/GitDemo.java
+++ src/main/java/com/tencent/GitDemo.java
@@ -11,7 +11,7 @@ public class GitDemo {
 
     public static void main(String[] args) {
 
-        System.out.println("hello betty");
+        System.out.println("hello tony");
         System.out.println("hello passion");
 
         System.out.println("commit and checkout");
