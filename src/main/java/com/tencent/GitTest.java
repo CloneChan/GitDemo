045aa40f02754c0af48cf18d565c366f1597d0fe
CloneChan <80335463+CloneChan@users.noreply.github.com>
2021-05-02 12:27:09 +0800

    Update GitTest.java
    git remote commit

--- src/main/java/com/tencent/GitTest.java
+++ src/main/java/com/tencent/GitTest.java
@@ -11,7 +11,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("aa");
         System.out.println("bb");
-        System.out.println("zhuiqufeng");
+        System.out.println("connnnn");
         System.out.println("cc");
 
     }
