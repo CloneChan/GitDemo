4c9503d7ee13b97c183870b79a35f5490cf530ee
CloneChan <80335463+CloneChan@users.noreply.github.com>
2021-05-02 09:58:22 +0800

    Create helloword.java
    commit from remote

---
+++ src/main/java/com/tencent/helloword.java
@@ -0,0 +1,14 @@
+package com.tencent;
+
+/**
+ * helloword.java
+ *
+ * @Author: CZQ
+ * @CreateTime: 9:54-2021/5/2
+ * @Description:
+ */
+public class helloword {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
