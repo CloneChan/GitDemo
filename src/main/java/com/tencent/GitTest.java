8c1bcee702d6f3685317fff5ff8483db1fc991a1
CloneChan <80335463+CloneChan@users.noreply.github.com>
2021-05-02 11:58:52 +0800

    Update GitTest.java
    the second commit from remote

--- src/main/java/com/tencent/GitTest.java
+++ src/main/java/com/tencent/GitTest.java
@@ -10,6 +10,10 @@ package com.tencent;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("aa");
+        System.out.println("bb");
+
+        System.out.println("cc");
+
     }
     private int aa;
 }
