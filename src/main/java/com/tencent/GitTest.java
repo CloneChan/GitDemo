464747afaf8be0ae89590f3b97eece1d8a656682
conan chan <15920025414@qq.com>
2021-05-02 12:29:52 +0800

    local GitTest uncommit java line16

--- src/main/java/com/tencent/GitTest.java
+++ src/main/java/com/tencent/GitTest.java
@@ -11,7 +11,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("aa");
         System.out.println("bb");
-        System.out.println("connnnn");
+        System.out.println("liuyujie");
         System.out.println("cc");
 
     }
